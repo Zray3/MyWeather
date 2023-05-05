a8cde64e4c9d680be3ca50ac803fa307da4debbc
ccremadesa <playerzray3@hotmail.com>
2023-05-05 19:12:43 +0200

    05.05

--- app/src/main/java/com/example/myweatherbase/activities/MainActivity.java
+++ app/src/main/java/com/example/myweatherbase/activities/MainActivity.java
@@ -1,43 +1,78 @@
 package com.example.myweatherbase.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myweatherbase.API.Connector;
 import com.example.myweatherbase.R;
 import com.example.myweatherbase.activities.model.Root;
 import com.example.myweatherbase.base.BaseActivity;
 import com.example.myweatherbase.base.CallInterface;
-import com.example.myweatherbase.base.ImageDownloader;
-import com.example.myweatherbase.base.Parameters;
+import com.google.android.material.snackbar.Snackbar;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends BaseActivity implements CallInterface {
 
-    private TextView txtView, textViewDay, textFecha, textDay, textTempAct, textTempMax, textTempMin ;
-    private ImageView imageView;
+    private TextView ciudadView;
+    private RecyclerView recyclerView;
     private Root root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_activity_main);
 
-        txtView = findViewById(R.id.txtView);
-        textViewDay = findViewById(R.id.textViewDay);
-        txtView = findViewById(R.id.txtView);
-        imageView = findViewById(R.id.imageView);
-        textDay = findViewById(R.id.textDay);
-        textFecha = findViewById(R.id.textFecha);
-        textTempAct = findViewById(R.id.textTempAct);
-        textTempMax = findViewById(R.id.textTempMax);
-        textTempMin = findViewById(R.id.textTempMin);
+        recyclerView = findViewById(R.id.entryRecycler);
+        ciudadView = findViewById(R.id.ciudadView);
+
+        MyRecyclerViewAdapter myRecyclerViewAdapter = new MyRecyclerViewAdapter(this, root);
+        recyclerView.setAdapter(myRecyclerViewAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
         // Mostramos la barra de progreso y ejecutamos la llamada a la API
         showProgress();
         executeCall(this);
     }
+
+    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            int posTarget = target.getAdapterPosition();
+
+            Root.List entry = root.list.get(viewHolder.getAdapterPosition());
+            root.list.remove(entry);
+            root.list.add(posTarget, entry );
+            recyclerView.getAdapter().notifyItemMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+            return true;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+            Root.List entry = root.list.get(viewHolder.getAdapterPosition());
+            root.list.remove(entry);
+            root.list.add(position, entry );
+            myRecyclerViewAdapter.notifyItemRemoved(position);
+            Snackbar.make(recyclerView, "Deleted " + entry.dt_txt,Snackbar.LENGTH_LONG)
+                    .setAction("Undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            root.list.add(position, entry);
+                            myRecyclerViewAdapter.notifyItemInserted(position);
+                        }
+                    }).show();
+        }
+    });
+
 
     // Realizamos la llamada y recogemos los datos en un objeto Root
     @Override
@@ -49,19 +84,6 @@ public class MainActivity extends BaseActivity implements CallInterface {
     @Override
     public void doInUI() {
         hideProgress();
-        txtView.setText(root.list.get(0).weather.get(0).description);
-        ImageDownloader.downloadImage(Parameters.ICON_URL_PRE + root.list.get(0).weather.get(0).icon + Parameters.ICON_URL_POST, imageView);
 
-        Date date = new Date((long)root.list.get(0).dt*1000);
-        SimpleDateFormat dateDayOfWeek = new SimpleDateFormat("EEEE");
-        SimpleDateFormat dateComplete = new SimpleDateFormat("d/MMMM/yyyy");
-        SimpleDateFormat dateHour = new SimpleDateFormat("HH:mm"); // "EEE, d MMM yyyy HH:mm"
-        textTempAct.setText("Temp: " +root.list.get(0).main.temp+"ºC");
-        textTempMax.setText("TempMax: " +root.list.get(0).main.temp_max+"ºC");
-        textTempMin.setText("TempMin: " +root.list.get(0).main.temp_min+"ºC");
-
-        textDay.setText(dateDayOfWeek.format(date));
-        textFecha.setText(dateComplete.format(date));
-        textViewDay.setText(dateHour.format(date));
     }
 }
--- app/src/main/java/com/example/myweatherbase/activities/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/myweatherbase/activities/MyRecyclerViewAdapter.java
@@ -10,18 +10,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myweatherbase.API.Connector;
 import com.example.myweatherbase.R;
 import com.example.myweatherbase.activities.model.Root;
+import com.example.myweatherbase.base.ImageDownloader;
+import com.example.myweatherbase.base.Parameters;
 
-public class MyRecyclerViewAdapter {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
 
     private Root root; //Lista de usuarios a mostrar
     //    private List<Usuario> usuarios;
     private final LayoutInflater inflater;
 
-    public MyRecyclerViewAdapter(Context context) {
-        root = UsuarioRepository.getInstance();
+    public MyRecyclerViewAdapter(Context context, Root root) {
+        this.root = Connector.getConector().get(Root.class,"&lat=39.4913900&lon=-0.4634900");
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
     }
 
 //    public MyRecyclerViewAdapter(Context context, List<Usuario> usuarios) {
@@ -41,31 +49,47 @@ public class MyRecyclerViewAdapter {
     // Usando como base el ViewHolder y lo personalizamos
     @Override
     public void onBindViewHolder(@NonNull MyRecyclerViewAdapter.ViewHolder holder, int position) {
-        Usuario usuario = listaUsuarios.get(position);
-        Profesion profesion = ProfesionRepository.getInstance().getProfesionByImage(usuario.getIdProfesion());
-        holder.nombre.setText(usuario.getApellidos() + ", " + usuario.getNombre());
-        holder.profesion.setText(profesion.getNombre());
-        holder.imagen.setImageResource(profesion.getImg());
+
+
+
+        holder.txtView.setText(root.list.get(0).weather.get(0).description);
+        ImageDownloader.downloadImage(Parameters.ICON_URL_PRE + root.list.get(0).weather.get(0).icon + Parameters.ICON_URL_POST, holder.imageView);
+
+        Date date = new Date((long)root.list.get(0).dt*1000);
+        SimpleDateFormat dateDayOfWeek = new SimpleDateFormat("EEEE");
+        SimpleDateFormat dateComplete = new SimpleDateFormat("d/MMMM/yyyy");
+        SimpleDateFormat dateHour = new SimpleDateFormat("HH:mm"); // "EEE, d MMM yyyy HH:mm"
+        holder.textTempAct.setText("Temp: " +root.list.get(0).main.temp+"ºC");
+        holder.textTempMax.setText("TempMax: " +root.list.get(0).main.temp_max+"ºC");
+        holder.textTempMin.setText("TempMin: " +root.list.get(0).main.temp_min+"ºC");
+
+        holder.textDay.setText(dateDayOfWeek.format(date));
+        holder.textFecha.setText(dateComplete.format(date));
+        holder.textViewDay.setText(dateHour.format(date));
     }
 
     // Indicamos el número de elementos de la lista
     @Override
     public int getItemCount() {
-        return listaUsuarios.size();
+        return root.list.size();
     }
 
     //Creamos nuestro ViewHolder, con los tipos de elementos a modificar
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView nombre;
-        private TextView profesion;
-        private ImageView imagen;
-
+        private TextView txtView, textViewDay, textFecha, textDay, textTempAct, textTempMax, textTempMin ;
+        private ImageView imageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            nombre = itemView.findViewById(R.id.textViewNombre);
-            profesion = itemView.findViewById(R.id.textViewOficio);
-            imagen = itemView.findViewById(R.id.imageView);
+            txtView = itemView.findViewById(R.id.txtView);
+            textViewDay = itemView.findViewById(R.id.textViewDay);
+            txtView = itemView.findViewById(R.id.txtView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textDay = itemView.findViewById(R.id.textDay);
+            textFecha = itemView.findViewById(R.id.textFecha);
+            textTempAct = itemView.findViewById(R.id.textTempAct);
+            textTempMax = itemView.findViewById(R.id.textTempMax);
+            textTempMin = itemView.findViewById(R.id.textTempMin);
         }
     }
 
